d4a6a2c5a88141af123e7cbfddad736d90190415
ndoumbe <thiawndoumbe@gmail.comgit config --global user.email thiawndoumbe@gmail.com>
2022-05-23 14:05:17 +0000

    correction exercice9

---
+++ exercice01.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 23 10:00:19 2022
+
+@author: ndoumbe
+"""
+"""
+# Exercice9
+coordinates=[(4,3),(9,3),(12,7),(13,7),(18,6),(20,19)]
+ordinates=[]
+dict_coordinate={}
+d2={}
+for i in coordinates :
+    c=(i[0])
+    v=i[1]
+    dict_coordinate[v]=c
+    d2={c+v}
+print(d2) 
+print(type(d2)) 
+"""
+  
+
+
+
+"""
+i=0
+dict_coordinates={}
+while i <
+coordinates=[(4,3),(9,3),(12,7),(13,7),(18,6),(20,7)]   
+i=0    
+while i< len(coordinates) :
+    tpl1=coordinates[i]
+    #print(tpl1[1])#
+    print(coordinates[i][1])
+    i=i+1
+"""  
+"""  
+coordinates=[(4,3),(9,3),(12,7),(13,7),(18,6),(20,19)]
+i=0
+dict_coordina
+while i< len(coordinates):
+    tpl1=coordinates[i]
+    c=tpl1[0]
+    v=tpl1[1]
+    dict_coordinates[c]=v
+    i=i+1
+print(dict_coordinates)   
+"""
+"""
+coordinates=[(4,5),(9,3),(12,8),(13,7),(18,6),(20,9)]
+i=0
+coordinates2=[]
+while i< len(coordinates):
+     point=coordinates[i]
+     y=point[1]
+     x=point[0]
+     if y>7:
+         nouveau_point=(x,7)
+     else:
+         nouveau_point=(x,y)
+     coordinates2.append(nouveau_point)    
+     i=i+1
+print(coordinates2)     
+"""
+"""
+coordinates=[(4,3),(9,3),(12,7),(13,7),(18,6),(20,19)]
+coordinates2=[]
+for i in coordinates :
+    y=i[1]
+    x=i[0]
+    if y>7:
+         nouveau_point=(x,7)
+    else:
+         nouveau_point=(x,y)
+    coordinates2.append(nouveau_point)    
+print(coordinates2) 
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
